0fd47a16fe10213857024ebc052c207e478d00e1
Anwar Muhamat <anwar.runs@gmail.com>
2017-10-02 21:12:24 +0000

    Change mongodb connection from 127.0.0.1:27017 to mongo

--- config/main.js
+++ config/main.js
@@ -3,7 +3,8 @@ module.exports = {
   secret: 'aOo4mV4wpzRKs4iht5p6ky58w7IrteiK',
   // Database connection information
   //database: 'mongodb://imanzahnurhidayat:hell0iB0y$2015@localhost:25097/collaboration',
-  database: 'mongodb://127.0.0.1:27017/collaboration',
+  // database: 'mongodb://127.0.0.1:27017/collaboration',
+  database: 'mongodb://mongo/collaboration',
   // Setting port for server
   port: 3000
 };

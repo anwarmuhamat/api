3b72477953354a59cc0050f58a1a4437cfab37c9
Anwar Muhamat <anwar.runs@gmail.com>
2017-10-02 05:42:33 +0000

    Change database connection

--- config/main.js
+++ config/main.js
@@ -2,7 +2,8 @@ module.exports = {
   // Secret key for JWT signing and encryption
   secret: 'aOo4mV4wpzRKs4iht5p6ky58w7IrteiK',
   // Database connection information
-  database: 'mongodb://imanzahnurhidayat:hell0iB0y$2015@localhost:25097/collaboration',
+  //database: 'mongodb://imanzahnurhidayat:hell0iB0y$2015@localhost:25097/collaboration',
+  database: 'mongodb://127.0.0.1:27017/collaboration',
   // Setting port for server
   port: 3000
 };

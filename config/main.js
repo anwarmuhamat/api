869135c44d6f051daebf6b41641e59fbb5b26d85
Anwar Muhamat <anwar.runs@gmail.com>
2017-10-12 08:14:34 +0700

    Change database name

--- config/main.js
+++ config/main.js
@@ -3,8 +3,8 @@ module.exports = {
   secret: 'aOo4mV4wpzRKs4iht5p6ky58w7IrteiK',
   // Database connection information
   //database: 'mongodb://imanzahnurhidayat:hell0iB0y$2015@localhost:25097/collaboration',
-  // database: 'mongodb://127.0.0.1:27017/collaboration',
-  database: 'mongodb://mongo/collaboration',
+  // database: 'mongodb://127.0.0.1:27017/blue',
+  database: 'mongodb://mongo/blue',
   // Setting port for server
   port: 3000
 };
